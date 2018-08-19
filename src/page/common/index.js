f45c455d069293dd1b2cede18f9c5b95e12185c6
zhaobw <1964471757@qq.com>
2018-08-19 13:43:17 +0800

    完成footer

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,10 +2,12 @@
  * @Author: zhaobowen 
  * @Date: 2018-08-11 18:04:42 
  * @Last Modified by: zhaobowen
- * @Last Modified time: 2018-08-14 01:03:48
+ * @Last Modified time: 2018-08-18 11:20:42
  */
 'use strict'
 // 因为每个页面都会引入common.js 所以这样在这里引入 公共的css 可以确保每个页面都有
 require('./layout.css');
 // fontawasome
 require('node_modules/font-awesome/css/font-awesome.min.css');
+// footer
+require('./footer/index.css');
